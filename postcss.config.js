bb6e9774c5c5ba6fa6f6c087fecff1fda914e3b9
marcograhl <{ID}+{username}@users.noreply.github.com>
2023-11-10 19:59:46 +0100

    added postcss plugins custom media and preset

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,21 @@
 export default {
 	plugins: {
 		tailwindcss: {},
-		autoprefixer: {}
+		'@csstools/postcss-global-data': {
+			files: [
+				'./src/design-tokens/media.css'
+			]
+
+		},
+		'postcss-custom-media':{},
+		'postcss-preset-env': {
+			stage: 3,
+			features: {
+				'nesting-rules': true,
+				'custom-media-queries': true,
+				'media-query-ranges': true,
+				'cascade-layers': false
+			}
+		},
 	}
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,4 +7,18 @@
 	class="bg-slate-700 hover:bg-slate-900 text-slate-50 h-10 flex justify-center items-center rounded-lg mt-10"
 >
 	{i('pages.home.goToTodoApp')}
+
 </a>
+
+	<a href="www.google.com">link</a>
+
+
+<style lang="postcss">
+	
+a{
+    font-size: 2rem;
+    @media(--above_med){
+        font-size: 5rem;
+    }
+}
+</style>
