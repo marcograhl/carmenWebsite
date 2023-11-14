98215db075b9c9baf6409287ade559692d3fa38c
marcograhl <{ID}+{username}@users.noreply.github.com>
2023-11-14 10:26:28 +0100

    header base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,27 +2,6 @@
 	import { i } from '@inlang/sdk-js';
 </script>
 
-<a
-	href="/todo"
-	class="bg-slate-700 hover:bg-slate-900 text-slate-50 h-10 flex justify-center items-center rounded-lg mt-10"
->
-	{i('pages.home.goToTodoApp')}
-
-</a>
-
-	<a href="www.google.com">link</a>
-
-<div class="parent">
-
-		<div class="child">
-
-			<div class="child-2 font-base">
-			 test
-
-			</div>
-		</div>
-</div>
-
 
 <style lang="postcss">
 
