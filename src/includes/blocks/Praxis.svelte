c41d1e40cd6f13548405703227e51addf44df2ad
marcograhl <{ID}+{username}@users.noreply.github.com>
2023-11-14 22:07:47 +0100

    hero section done startseite

--- src/includes/blocks/Praxis.svelte
+++ src/includes/blocks/Praxis.svelte
@@ -3,21 +3,23 @@
 </script>
 
 <article class="">
-  <address class="">
-    <div class="">
-      <!-- <img class="self-center card-plant" src={imgCardPlant} alt="deco" width="53" height="104"> -->
-      <p class="">
-        <span class="">{@html i('Startseite.card.0')}</span>
-        <span class="">{@html i('Startseite.card.1')}</span>
-      </p>
-    </div>
-    <div class="">
-      <span class="">{i('Startseite.card.2')}</span>
-      <a class="" target='_blank' rel='noreferrer' href='https://www.google.com/maps/place/Psychologische+Privatpraxis+im+Bayerischen+Viertel+Carmen+Alvarez/@52.4869995,13.3383774,15z/data=!4m6!3m5!1s0x47a851faa4a2d3a3:0xba7343ea7b73ad4d!8m2!3d52.4869995!4d13.3383774!16s%2Fg%2F11sjgtq6tx?entry=ttu' >Bozener Str. 17, 10825 Berlin</a>
-    </div>
-    <div class="">
-      <span class="">e&#8211;mail:</span>
-      <a class="" href="mailto:alvarez.carmenglo@gmail.com">alvarez.carmenglo@gmail.com</a>
-    </div>
-  </address>
+  <div class="wrapper">
+    <address class="">
+      <div class="">
+        <!-- <img class="self-center card-plant" src={imgCardPlant} alt="deco" width="53" height="104"> -->
+        <p class="">
+          <span class="">{@html i('Startseite.card.0')}</span>
+          <span class="">{@html i('Startseite.card.1')}</span>
+        </p>
+      </div>
+      <div class="">
+        <span class="">{i('Startseite.card.2')}</span>
+        <a class="" target='_blank' rel='noreferrer' href='https://www.google.com/maps/place/Psychologische+Privatpraxis+im+Bayerischen+Viertel+Carmen+Alvarez/@52.4869995,13.3383774,15z/data=!4m6!3m5!1s0x47a851faa4a2d3a3:0xba7343ea7b73ad4d!8m2!3d52.4869995!4d13.3383774!16s%2Fg%2F11sjgtq6tx?entry=ttu' >Bozener Str. 17, 10825 Berlin</a>
+      </div>
+      <div class="">
+        <span class="">e&#8211;mail:</span>
+        <a class="" href="mailto:alvarez.carmenglo@gmail.com">alvarez.carmenglo@gmail.com</a>
+      </div>
+    </address>
+  </div>
 </article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,82 @@
 <script>
 	import { i } from '@inlang/sdk-js';
-	import HeroWrapper from "$blocks/HeroWrapper.svelte";
-	import HeroImg from "$assets/heroimg.png";
-	import PraxisSection from "$blocks/Praxis.svelte";
+	import HeroWrapper from '$blocks/HeroWrapper.svelte';
+	import HeroImg from '$assets/heroimg.png';
+	import PraxisSection from '$blocks/Praxis.svelte';
 	import QuoteSection from '$blocks/Quote.svelte';
 </script>
 
-
 <HeroWrapper>
 	<div class="sidebar">
-		<div class="headline">
-			<h1>{@html i('Startseite.heading')}</h1>
-			<p>{i('Startseite.subheading')}<p>
+		<div class="left center-xy">
+			<div class="heading-wrapper flow">
+				<h1 class="heading">
+					{i('Startseite.title.0')}
+					<span class="indent-line">
+						{i('Startseite.title.1')}
+					</span>
+					{i('Startseite.title.2')}
+				</h1>
+				<p>{i('Startseite.subheading')}</p>
+			</div>
 		</div>
-		<div class="img-wrapper">
-        <img class="mx-auto" src={HeroImg} alt="testing" width="322" height="400">
+		<div class="right">
+			<img class="mx-auto" src={HeroImg} alt="testing" width="322" height="400" />
 		</div>
 	</div>
 </HeroWrapper>
 
-<PraxisSection/>
-<QuoteSection/>
-
-
-
+<!-- <PraxisSection /> -->
+<!-- <QuoteSection /> -->
 
 <style lang="postcss">
+	
+	.heading-wrapper{
+		margin-bottom: 1.5ex;
+
+	}
+
+	.heading{
+		line-height: 1.1;
+		@media(--above_large){
+		display: grid;
+    max-width: 28ch;
+		}
+	}
+
+	img{
+    width: clamp(14.5rem, 13.5rem + 12vw, 20.15rem);
+	}
+
+
 
 	
+
+	 
+	.indent-line {
+		@media (--above_large) {
+			text-indent: 1.5ch;
+		}
+	}
+
+	.sidebar {
+		--sidebar-vertical-alignment: flex-start;
+		--sidebar-target-width: 32.5rem;
+		--sidebar-content-min-width: 25%;
+		--gutter: var(--space-m-3xl);
+	}
+
+	p {
+    max-width: 55ch;
+		font-size: var(--size-step-2);
+    @media(--above_large){
+     text-indent: 1ch;
+  }
+	}
+
+	:global(.indent-line) {
+		@media (--above_large) {
+			text-indent: 1.5ch;
+		}
+	}
 </style>
