7d42dace65a21382071727e022f2a70a83baa342
marcograhl <{ID}+{username}@users.noreply.github.com>
2023-11-14 11:08:24 +0100

    html structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,17 @@
 <script>
 	import { i } from '@inlang/sdk-js';
+	import HeroWrapper from "$blocks/HeroWrapper.svelte"
 </script>
+
+
+<HeroWrapper>
+	<div class="sidebar">
+		<div class="headline">
+			<h1>{@html i('Startseite.heading')}</h1>
+			<p>{i('Startseite.subheading')}<p>
+		</div>
+	</div>
+</HeroWrapper>
 
 
 <style lang="postcss">
